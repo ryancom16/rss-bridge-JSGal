8227244b55aa70f93b6e255dee4eca6816cdaa66
ryancom16 <Ryancom16@gmail.com>
2023-11-11 09:25:51 -0700

    Fix Alt descriptions for images
    Pulls the alt from each image director or the Title of the post if the alt does not exist.

--- templates/gallery-format.html.php
+++ templates/gallery-format.html.php
@@ -5,15 +5,19 @@
     <meta charset="<?= htmlspecialchars($charset, ENT_QUOTES, 'UTF-8') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="RSS-Bridge" />
-    <title><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></title>
+    <title>
+        <?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?>
+    </title>
     <link href="static/style.css?2023-03-24" rel="stylesheet">
     <link rel="icon" type="image/png" href="static/favicon.png">
     <link rel="stylesheet" href="static/css/justifiedGallery.min.css" />
     <script src="static/js/jquery.min.js"></script>
     <script src="static/js/jquery.justifiedGallery.min.js"></script>
 
-    <?php foreach ($linkTags as $link) : ?>
-        <link href="<?= htmlspecialchars($link['href'], ENT_QUOTES, 'UTF-8') ?>" title="<?= htmlspecialchars($link['title'], ENT_QUOTES, 'UTF-8') ?>" rel="alternate" type="<?= htmlspecialchars($link['type'], ENT_QUOTES, 'UTF-8') ?>">
+    <?php foreach ($linkTags as $link): ?>
+        <link href="<?= htmlspecialchars($link['href'], ENT_QUOTES, 'UTF-8') ?>"
+            title="<?= htmlspecialchars($link['title'], ENT_QUOTES, 'UTF-8') ?>" rel="alternate"
+            type="<?= htmlspecialchars($link['type'], ENT_QUOTES, 'UTF-8') ?>">
     <?php endforeach; ?>
 
     <meta name="robots" content="noindex, follow">
@@ -24,7 +28,9 @@
     <div class="container">
 
         <h1 class="pagetitle">
-            <a href="<?= htmlspecialchars($uri, ENT_QUOTES, 'UTF-8') ?>" target="_blank"><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></a>
+            <a href="<?= htmlspecialchars($uri, ENT_QUOTES, 'UTF-8') ?>" target="_blank">
+                <?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?>
+            </a>
         </h1>
 
         <div class="buttons">
@@ -32,28 +38,33 @@
                 <button class="backbutton">← back to rss-bridge</button>
             </a>
 
-            <?php foreach ($buttons as $button) : ?>
+            <?php foreach ($buttons as $button): ?>
                 <a href="<?= htmlspecialchars($button['href'], ENT_QUOTES, 'UTF-8') ?>">
-                    <button class="rss-feed"><?= htmlspecialchars($button['value'], ENT_QUOTES, 'UTF-8') ?></button>
+                    <button class="rss-feed">
+                        <?= htmlspecialchars($button['value'], ENT_QUOTES, 'UTF-8') ?>
+                    </button>
                 </a>
             <?php endforeach; ?>
         </div>
     </div>
 
     <!--Justified Gallery -->
-    <?php foreach ($items as $index => $item) : ?>
+    <?php foreach ($items as $index => $item): ?>
         <div class="item-container">
-            <?php if ($item['title']) : ?>
+            <?php if ($item['title']): ?>
                 <section class="feeditem">
                     <h2>
-                        <a class="itemtitle" href="<?= e($item['url']) ?>"><?= strip_tags($item['title']) ?></a>
+                        <a class="itemtitle" href="<?= e($item['url']) ?>">
+                            <?= strip_tags($item['title']) ?>
+                        </a>
                     </h2>
                 </section>
             <?php endif; ?>
             <div id="justified-gallery-<?= $index ?>" class="justified-gallery">
-                <?php foreach ($item['galleryItems'] as $galleryItem) : ?>
+                <?php foreach ($item['galleryItems'] as $galleryItem): ?>
                     <a href="<?= htmlspecialchars($galleryItem['url'], ENT_QUOTES, 'UTF-8') ?>" class="gallery-item">
-                        <img src="<?= htmlspecialchars($galleryItem['thumbnail'], ENT_QUOTES, 'UTF-8') ?>" alt="<?= htmlspecialchars($item['title'], ENT_QUOTES, 'UTF-8') ?>">
+                        <img src="<?= htmlspecialchars($galleryItem['thumbnail'], ENT_QUOTES, 'UTF-8') ?>"
+                            alt="<?= htmlspecialchars($galleryItem['alt'], ENT_QUOTES, 'UTF-8') ?>">
                     </a>
                 <?php endforeach; ?>
             </div>
@@ -62,8 +73,8 @@
 
     </div>
     <script>
-        $(document).ready(function() {
-            $('.justified-gallery').each(function() {
+        $(document).ready(function () {
+            $('.justified-gallery').each(function () {
                 $(this).justifiedGallery({
                     rowHeight: <?= htmlspecialchars($gallerySize, ENT_QUOTES, 'UTF-8') ?>,
                     lastRow: 'nojustify',
